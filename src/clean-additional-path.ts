04173e2cd01b7f317b7176ab27e9a3336364cb97
이승렬 <toy0605@gmail.com>
2021-12-19 23:00:02 +0900

    Fix support missing home directory

--- src/clean-additional-path.ts
+++ src/clean-additional-path.ts
@@ -2,13 +2,18 @@ import * as core from "@actions/core";
 import { readdir, rm, stat } from "fs/promises";
 import { existsSync as exists } from "fs";
 import * as path from "path";
-import * as util from "util";
+import * as os from "os";
 import { sync as glob } from "glob";
 
 export async function cleanAdditionalPath(additionalPathInput: string) {
+  const homePath = os.homedir();
   const additionalPaths = additionalPathInput.split(",");
   for (const rawAdditionalPath of additionalPaths) {
-    const globPaths = glob(rawAdditionalPath);
+    const tempPath = path.join(
+      rawAdditionalPath.replace(/^[~]/g, ""),
+      homePath
+    );
+    const globPaths = glob(tempPath);
     for (const additionalPath of globPaths) {
       if (exists(additionalPath) === false) {
         continue;
