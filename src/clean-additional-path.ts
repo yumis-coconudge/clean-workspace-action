a60cac11eb464d4bb60b9f65260130cb77c606b3
이승렬 <toy0605@gmail.com>
2021-12-19 22:56:02 +0900

    Add support glob pattern

--- src/clean-additional-path.ts
+++ src/clean-additional-path.ts
@@ -2,31 +2,32 @@ import * as core from "@actions/core";
 import { readdir, rm, stat } from "fs/promises";
 import { existsSync as exists } from "fs";
 import * as path from "path";
+import * as util from "util";
+import { sync as glob } from "glob";
 
 export async function cleanAdditionalPath(additionalPathInput: string) {
   const additionalPaths = additionalPathInput.split(",");
   for (const rawAdditionalPath of additionalPaths) {
-    const additionalPath = rawAdditionalPath.replace(
-      /^[~][/]/g,
-      path.join(process.env.HOME ?? "", "/")
-    );
-    if (exists(additionalPath) === false) {
-      continue;
-    }
+    const globPaths = glob(rawAdditionalPath);
+    for (const additionalPath of globPaths) {
+      if (exists(additionalPath) === false) {
+        continue;
+      }
 
-    const fileStat = await stat(additionalPath);
-    if (fileStat.isDirectory() === true) {
-      const additionalFiles = await readdir(additionalPath);
-      await Promise.all(
-        additionalFiles.map((file) =>
-          rm(path.join(additionalPathInput, file), {
-            recursive: true,
-            force: true,
-          })
-        )
-      );
-    } else {
-      await rm(additionalPath, { force: true });
+      const fileStat = await stat(additionalPath);
+      if (fileStat.isDirectory() === true) {
+        const additionalFiles = await readdir(additionalPath);
+        await Promise.all(
+          additionalFiles.map((file) =>
+            rm(path.join(additionalPathInput, file), {
+              recursive: true,
+              force: true,
+            })
+          )
+        );
+      } else {
+        await rm(additionalPath, { force: true });
+      }
     }
   }
   core.info("Cleaned additional path.");
